55b283b526aa19abb3dfad225a798882862036c4
amoshydra <amoswwj@gmail.com>
2017-09-12 15:16:39 +0800

    Fix navbar selection

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,13 +2,13 @@
   <nav class="navbar navbar-default">
     <div class="container">
       <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li class="active"><router-link to="/">Welcome</router-link></li>
-          <li><router-link to="/questionnaire-pre">Pre Questionnaire</router-link></li>
-          <li><router-link to="/instructions">Instructions</router-link></li>
-          <li><router-link to="/experiment">Experiment</router-link></li>
-          <li><router-link to="/questionnaire-post">Post Questionnaire</router-link></li>
-        </ul>
+        <div class="nav navbar-nav">
+          <router-link to="/">Welcome</router-link>
+          <router-link to="/questionnaire-pre">Pre Questionnaire</router-link>
+          <router-link to="/instructions">Instructions</router-link>
+          <router-link to="/experiment">Experiment</router-link>
+          <router-link to="/questionnaire-post">Post Questionnaire</router-link>
+        </div>
       </div>
     </div>
   </nav>
@@ -16,10 +16,25 @@
 
 <script>
 export default {
-
 };
 </script>
 
 <style>
+.navbar-nav::after {
+  clear: both;
+}
 
+.navbar-nav > a {
+  text-decoration: none;
+  color: #777;
+  display: block;
+  float: left;
+  height: 100%;
+  padding: 15px;
+}
+
+.router-link-exact-active {
+  color: #555;
+  background-color: #e7e7e7;
+}
 </style>
